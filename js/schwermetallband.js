21c1b05d41bed943ae2b738ae0488403fd1d7c39
schwermetallband <49091428+schwermetallband@users.noreply.github.com>
2022-09-14 15:29:56 +0200

    bug fixing

--- js/schwermetallband.js
+++ js/schwermetallband.js
@@ -99,9 +99,4 @@ document.addEventListener('play', function(curAudio){
 //toggle darke mode
 document.getElementById('themeSwitch').addEventListener('change', function(event) {
 	(event.target.checked) ? document.body.setAttribute('data-theme', 'dark') : document.body.removeAttribute('data-theme');
-	(event.target.checked) ? document.getElementById('smbLogo').src='../img/logoschwermetall.dark.jpg' : document.getElementById('smbLogo').src='../img/logoschwermetall.jpg';
-	//(event.target.checked) ? document.getElementById('G').src='../img/G.dark.PNG' : document.getElementById('G').src='../img/G.PNG';
-	//(event.target.checked) ? document.getElementById('M').src='../img/M.dark.PNG' : document.getElementById('M').src='../img/M.PNG';
-	//(event.target.checked) ? document.getElementById('H').src='../img/H.dark.PNG' : document.getElementById('H').src='../img/H.PNG';	
-	}
 );
